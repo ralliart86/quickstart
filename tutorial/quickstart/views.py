e6fae205127636d6428ab98bde50aa29a3a59e28
Pedro <pedro@ownedoutcomes.com>
2016-08-17 15:54:52 -0400

    Debbuging

--- tutorial/quickstart/views.py
+++ tutorial/quickstart/views.py
@@ -1,16 +1,14 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
 from tutorial.quickstart.serializers import UserSerializer, GroupSerializer
-from django.shortcuts import render
 
-# Create your views here.
 
 class UserViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows users to be viewed or edited.
     """
     queryset = User.objects.all().order_by('-date_joined')
-    serializers_class = UserSerializer
+    serializer_class = UserSerializer
 
 
 class GroupViewSet(viewsets.ModelViewSet):
@@ -18,4 +16,4 @@ class GroupViewSet(viewsets.ModelViewSet):
     API endpoint that allows groups to be viewed or edited.
     """
     queryset = Group.objects.all()
-    serializers_class = GroupSerializer
+    serializer_class = GroupSerializer
